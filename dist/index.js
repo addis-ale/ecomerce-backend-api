c5cb1f77a75ab512b42ec11a2400460004438e68
addisalex07 <addis.ugr-4143-15@aau.edu.et>
2025-02-14 19:45:41 +0300

    prisma and mysql setup completed

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,44 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+// Get all users
 app.get("/", (req, res) => {
-    res.send("yes,working");
+    res.send("hello");
 });
-app.listen(process.env.PORT || 8001, () => {
-    console.log("app working");
+app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield prisma.user.findMany();
+    res.json(users);
+}));
+// Create a new user
+app.post("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name, email, password } = req.body;
+    try {
+        const newUser = yield prisma.user.create({
+            data: { name, email, password },
+        });
+        res.json(newUser);
+    }
+    catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}));
+// Start the server
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 const app = express();
+app.use(express.json());
+
+// Get all users
 app.get("/", (req, res) => {
-  res.send("yes,working");
+  res.send("hello");
 });
-app.listen(process.env.PORT || 8001, () => {
-  console.log("app working");
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+// Create a new user
+app.post("/users", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const newUser = await prisma.user.create({
+      data: { name, email, password },
+    });
+    res.json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: (error as any).message });
+  }
+});
+
+// Start the server
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
