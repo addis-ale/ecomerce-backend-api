276f35308307ed543a0d6eefea7e31102e514c92
addisalex07 <addis.ugr-4143-15@aau.edu.et>
2025-02-14 15:23:15 +0300

    tsc configuration setup done

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.get("/", (req, res) => {
-    res.send("working");
+    res.send("yes,working");
 });
-app.listen(3000, () => {
+app.listen(process.env.PORT || 8001, () => {
     console.log("app working");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.get("/", (req, res) => {
-  res.send("working");
+  res.send("yes,working");
 });
-app.listen(3000, () => {
+app.listen(process.env.PORT || 8001, () => {
   console.log("app working");
 });
